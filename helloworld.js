d8e488f0d762d6212411d156308529705bad815b
my-uri <yurimartins18@gmail.com>
2026-07-07 11:03:07 -0300

    iniciando js

---
+++ helloworld.js
@@ -0,0 +1,19 @@
+var nome = 'oswaldo'
+var idade = 39
+var altura = 1.63
+var peso = 77
+var doadorDeSangue = true
+var tipoSanguineo = 'a'
+var fatorRH = '+'
+
+console.log('nome: ', nome)
+console.log('idade: ', idade)
+console.log('altura: ', altura)
+console.log('peso: ', peso)
+console.log('Doador de Sangue: ', doadorDeSangue)
+console.log('Tipo Sanguineo: ', tipoSanguineo)
+console.log('Fator Rh: ', fatorRH)
+
+console.log(' (\\_(\\')
+console.log(' (=\';\')')
+console.log(',((\")(\")')
---
+++ index.js
@@ -0,0 +1,4 @@
+let nota1 = 8, nota2 = 6
+let media = (nota1 + nota2) % 2
+
+console.log(media)
